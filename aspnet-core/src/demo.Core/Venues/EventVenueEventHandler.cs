bbc49abe99b779df86855f356565803709f86d46
Alexious Yong <46164655+ebiggerr@users.noreply.github.com>
2021-07-27 13:55:36 +0800

    Added a domain event handler for the event and venue ( validation on venue when user wants to create a new event)

---
+++ aspnet-core/src/demo.Core/Venues/EventVenueEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp;
+using Abp.Dependency;
+using Abp.Domain.Uow;
+using Abp.Events.Bus.Handlers;
+using Abp.Threading;
+using demo.Events;
+
+namespace demo.Venues
+{
+    public class EventVenueEventHandler : AbpServiceBase, IEventHandler<VenueBookedEvent>, ITransientDependency
+    {
+        private readonly VenueManager _venueManager;
+        private readonly EventManager _eventManager;
+
+        public EventVenueEventHandler(VenueManager venueManager,EventManager eventManager)
+        {
+            _venueManager = venueManager;
+            _eventManager = eventManager;
+        }
+
+        [UnitOfWork]
+        public virtual void HandleEvent(VenueBookedEvent eventData)
+        {
+            AsyncHelper.RunSync(async () => await HandleBookingToAVenue(eventData.VenueId));
+        }
+
+        private async Task HandleBookingToAVenue(Guid eventDataVenueId)
+        {
+            await _venueManager.CheckVenueAsync(eventDataVenueId);
+        }
+    }
+}
